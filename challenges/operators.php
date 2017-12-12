e991a7bc48e320c4adec011ef98dd160d3a465d7
Jochem Kossen <jkossen@landstede.nl>
2017-12-12 18:02:47 +0100

    Update

--- challenges/operators.php
+++ challenges/operators.php
@@ -10,8 +10,22 @@ $b = false;
 // Schrijf hier een if statement die "a OF b is waar" op het scherm toont als
 // $a OF $b waar is (maar niet allebei)
 
+
 /**
  * Opdracht 2
+ *
+ * Onderstaand if statement is niet goed. Corrigeer het.
+ */
+
+$a = 0;
+$b = false;
+
+if ($a = $b) {
+    echo "a is gelijk aan aan b";
+}
+
+/**
+ * Opdracht 3
  *
  * Onderstaand if statement is niet goed. Corrigeer het.
  */
@@ -22,4 +36,3 @@ $b = false;
 if ($a == $b) {
     echo "a is identiek aan b";
 }
-
