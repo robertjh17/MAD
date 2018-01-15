d0fb09e64f65ba36c538e586de04fe38f2c1983e
Jochem Kossen <jkossen@landstede.nl>
2018-01-15 10:34:48 +0100

    Voor tijdens de les

---
+++ challenges/scissors.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Tussen BEGIN en END staat een volledig PHP script. Alleen, mijn zoontje van 3
+ * heeft het helaas verknipt en door elkaar gegooid. Ik kan er niks meer mee,
+ * dus ik hoop dat jullie dit kunnen fixen!
+ *
+ * Gebruik ALLEEN de regels code tussen BEGIN en END. Je mag de regels code NIET
+ * aanpassen.
+ */
+
+
+/* BEGIN
+}
+
+
+}
+function getMessage()
+{
+
+<?php
+function decodeMessage($encoded_message)
+    $msg = "Tbrq trqnna! Qvg yvwxg rebc!";
+
+
+    return str_rot13($encoded_message);
+{
+    echo getMessage();
+    return decodeMessage($msg);
+
+END */
